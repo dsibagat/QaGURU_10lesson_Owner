5223fe36958e4ba1cc6a8aca1258647015547692
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-03-09 19:47:52 +0300

    add test

---
+++ src/test/java/tests/SimpleTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import config.ConfigHelper;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleTest {
+    @Test
+    void selenideSearchTest() {
+        Configuration.remote = ConfigHelper.getWebdriverRemote();
+
+        String searchSite = ConfigHelper.getSearchSite();
+        String searchItem = ConfigHelper.getSearchItem();
+        String searchResult = ConfigHelper.getSearchResult();
+        String password = ConfigHelper.getPassword();
+        System.out.println(password);
+
+        open(searchSite);
+
+        $(byName("q")).setValue(searchItem).pressEnter();
+
+        $("html").shouldHave(text(searchResult));
+    }
+}
